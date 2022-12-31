8413861f124a5636c4b3a26530d204a374581739
Matan Yemini <matan@engageli.com>
2022-12-31 12:21:50 +0200

    update circles component

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -1,15 +1,29 @@
+import { motion } from "framer-motion";
 import React from "react";
 
 type Props = {};
 
 const BackgroundCircles = (props: Props) => {
     return (
-        <div className="relative flex justify-center items-center">
+        <motion.div
+            initial={{
+                opacity: 0,
+            }}
+            animate={{
+                scale: [1, 2, 2, 3, 1],
+                opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
+                borderRadius: ["20%", "20%", "50%", "80%", "20%"],
+            }}
+            transition={{
+                duration: 2.5,
+            }}
+            className="relative flex justify-center items-center"
+        >
             <div className="absolute border border-[#333333] rounded-full h-48 w-48 mt-52 animate-ping" />
             <div className="absolute border border-[#333333] rounded-full h-64 w-64 mt-52 " />
             <div className="absolute border border-[#333333] rounded-full h-96 w-96 mt-52 " />
             <div className="rounded-full border border-[#F7AB0A] opacity-20 h-[600px] w-[600px] mt-52 animate-pulse absolute" />
-        </div>
+        </motion.div>
     );
 };
 
