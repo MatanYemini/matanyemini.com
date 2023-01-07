77a35ac91b1021f72854f0e1cf94027b185d0e00
Matan Yemini <matan@engageli.com>
2023-01-07 13:51:34 +0200

    add minifier

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ["matanyemini.s3.eu-north-1.amazonaws.com"],
+        domains: ["matanyemini.s3.eu-north-1.amazonaws.com", "d2ldb6hpndi77k.cloudfront.net"],
+        minimumCacheTTL: 60 * 60, // 1hr
     },
+    swcMinify: true,
 };
 
 module.exports = nextConfig;
