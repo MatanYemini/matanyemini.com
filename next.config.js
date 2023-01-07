aa0d432f2e3d492a8a00bfa305185aee84ef2636
Matan Yemini <matan@engageli.com>
2023-01-07 17:11:06 +0200

    increase cacheTTL

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
         domains: ["matanyemini.s3.eu-north-1.amazonaws.com", "d2ldb6hpndi77k.cloudfront.net"],
-        minimumCacheTTL: 60 * 60, // 1hr
+        minimumCacheTTL: 60 * 60 * 24 * 14, // 14days
     },
     swcMinify: true,
 };
