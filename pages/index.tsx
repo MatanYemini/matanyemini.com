f066dc4612055d5eb5e3503fbbbc0d4fb2338fb3
Matan Yemini <matan@engageli.com>
2023-01-14 14:27:36 +0200

    add google verification tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,7 @@ export default function Home() {
                 <meta name="description" content="Matan Yemini - Software and Product" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta name="keywords" content="Matan Yemini,Software,Product,Startups" />
+                <meta name="google-site-verification" content="MQkqrrrRxNbHOt3n5AdYdjlx0-oVOhQBuvfe5pDGmpA" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className="overflow-x-hidden overflow-y-scroll bg-[#242424] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 scrollbar-thin">
